754e706b677e280d884a6c22fbae68fd85107923
hahamdaoui <hakimhamdawi1^gmail.com>
2023-03-19 13:26:05 -0700

    duote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A program that printfs a line to the standard error  
+ * main - A c program that printfs a line to the standard error
  *
  * Return: 1 (Success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r",stdout);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
 	return (1);
 }
