89fe58a69064cafaf3a1a5c569f26c908ada2622
hahamdaoui <hakimhamdawi1^gmail.com>
2023-03-19 11:13:24 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line wiyh puts
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
