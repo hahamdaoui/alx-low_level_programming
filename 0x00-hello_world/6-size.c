dfacc0374eeeab6daa206dc57f1da22fa14cfeaf
hahamdaoui <hakimhamdawi1^gmail.com>
2023-03-19 12:52:49 -0700

    size of type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,12 @@
  */
 int main(void)
 {
-	char A;
+	char a;
 	int b;
 	long int c;
 	long long int d;
 	float f;
+
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
