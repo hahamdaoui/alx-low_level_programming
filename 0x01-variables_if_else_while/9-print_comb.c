57335d0f792a42a362af824ba89b1ea5fa95fd2c
hahamdaoui <hakimhamdawi1^gmail.com>
2023-03-19 17:22:15 -0700

    Patience persistence and perspiration an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always (0) (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0' ; i <= '9' ; i++)
+	{
+		putchar(i);
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
