55bb125f5d2cab2deb6cf8c7911b816f9707a505
hahamdaoui <hakimhamdawi1^gmail.com>
2023-03-19 17:06:22 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: amways (0) (Success)
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
