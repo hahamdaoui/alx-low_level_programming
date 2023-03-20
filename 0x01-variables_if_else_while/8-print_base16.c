86f2d1ed9936c41d7eef9df3ec8519d70f2e9aca
hahamdaoui <hakimhamdawi1^gmail.com>
2023-03-19 17:17:31 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always (0) (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0' ; i <= '9' ; i++)
+		putchar(i);
+	for (i = 'a' ; i <= 'f' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
