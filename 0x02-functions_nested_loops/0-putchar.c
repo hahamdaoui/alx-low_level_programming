5d0e785ff31e08c21abc5220ebbb3e8f52ce2a98
hahamdaoui <hakimhamdawi1^gmail.com>
2023-03-21 07:21:58 -0700

    print char

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,14 @@
-#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
 /**
- * main - A program will be printf this word _putchar
- *
- * Return: 0 (success)
+ * _puchar - A program will be printf this word _putchar
+ *@c: The character to print
+
+ * Return: On success 1
+ * On error, -1 is returned, and errno is set appropriately
  *
  */
-int main(void)
+int _puchar(char c)
 {
-	printf("putchar");
-	return (0);
+	return (write(1, &c, 1));
 }
