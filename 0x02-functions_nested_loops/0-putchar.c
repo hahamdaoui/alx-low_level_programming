a24d225f7c08a61b9c6e38d0b711936b5e574934
hahamdaoui <hakimhamdawi1^gmail.com>
2023-03-21 07:14:34 -0700

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A program will be printf this word _putchar
+ *
+ * Return: 0 (success)
+ *
+ */
+int main(void)
+{
+	printf("putchar");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+c - functions nrsted loops
