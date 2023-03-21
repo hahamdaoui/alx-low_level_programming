4d835cff4b6274e666ad8ca2eb9d911c33476306
hahamdaoui <hakimhamdawi1^gmail.com>
2023-03-21 08:07:10 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,19 @@
 #include "main.h"
-#include <unistd.h>
 /**
- * _puchar - A program will be printf this word _putchar
- *@c: The character to print
-
- * Return: On success 1
- * On error, -1 is returned, and errno is set appropriately
- *
+ * main - C progrem that print _puchar
+ * is written by hahamdaoui
+ * Return: 0 (success)
  */
-int _puchar(char c)
+int main(void)
 {
-	return (write(1, &c, 1));
+	_putchar('_');
+	_putchar('P');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
