3159c026ea6bf9caa10d46e06f79bbd1a023d356
hahamdaoui <hakimhamdawi1^gmail.com>
2023-03-21 08:46:01 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,15 @@
 #include "main.h"
+
 /**
  * main - C progrem that print _puchar
  * is written by hahamdaoui
  * Return: 0 (success)
  */
+
 int main(void)
 {
 	_putchar('_');
-	_putchar('P');
+	_putchar('p');
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabets
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		_putchar(i);
+	_putchar('\n');
+}
+
