e8c9945219cc85f7d059e17fff19b0d19b2a660d
hahamdaoui <hakimhamdawi1^gmail.com>
2023-03-27 11:24:50 -0700

    pointers arrays strings

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,11 @@
 #ifndef main_h
 #define main_h
 
+
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
