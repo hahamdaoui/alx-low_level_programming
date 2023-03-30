1dc7ef2797230dfd9e44bb1596cd722048bd2cd6
hahamdaoui <hakimhamdawi1^gmail.com>
2023-03-29 17:10:18 -0700

    update

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/*
+ * main - A program that return nomber magic
+ * @n : variable integer
+ * Return: 0 (Success)
+ */
 
 int main(void)
 {
@@ -6,7 +11,7 @@ int main(void)
 	int a[5];
 	int *p;
 
-	a[2] = 98;
+	a[2] = 1024;
 	p = &n;
  /*
   * write your line of code here...
@@ -16,7 +21,7 @@ int main(void)
   * - only one statement
   * - you are not allowed to code anything else than this line of code
   */
-  *(p + 5) = 98;
+	*(p + 5) = 98;
   /* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
