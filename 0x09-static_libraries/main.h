959a4ea7e1a64a03454255a03bcb36686b39254f
hahamdaoui <hakimhamdawi1^gmail.com>
2023-04-06 10:10:40 -0700

    add file main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,7 @@
 #ifndef main_h
 #define main_h
+
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -20,4 +22,6 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+
 #endif
