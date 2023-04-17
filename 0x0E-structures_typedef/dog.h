cdc6faff9ce51834e0fb46f740baeb34e12ad359
hahamdaoui <hakimhamdawi1^gmail.com>
2023-04-17 11:14:37 -0700

    creat dog stractuer

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
 #ifndef dog_h
-#define doh_h
+#define dog_h
 
 /**
  * struct dog - a dog's basic info
@@ -27,4 +27,5 @@ dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
+
 #endif
