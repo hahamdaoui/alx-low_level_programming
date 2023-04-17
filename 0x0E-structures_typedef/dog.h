91b2ebc06267a1f76dd9efb27688997b9558d8f6
hahamdaoui <hakimhamdawi1^gmail.com>
2023-04-17 11:08:02 -0700

    add structer dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,31 +1,30 @@
 #ifndef dog_h
 #define doh_h
+
 /**
-	 * struct dog - a dog's basic info
-	 * @name: First member
-	 * @age: Second member
-	 * @owner: Third member
-	 *
-	 * Description: Longer description
-	 */
-	struct dog
-	{
-		char *name;
-		float age;
-		char *owner;
-	};
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
 
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
 
-	/**
-	 * dog_t - typedef for struct dog
-	 */
-	typedef struct dog dog_t;
-
-
-	void init_dog(struct dog *d, char *name, float age, char *owner);
-	void print_dog(struct dog *d);
-	dog_t *new_dog(char *name, float age, char *owner);
-	void free_dog(dog_t *d);
-	char *_strcpy(char *dest, char *src);
-	int _strlen(char *s);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 #endif
